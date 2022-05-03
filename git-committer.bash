11a1a27595ff38d256dbd05828d530bcee40941a
BuildTools <unconfigured@null.spigotmc.org>
2022-05-03 17:46:47 -0400

    commit

--- git-committer.bash
+++ git-committer.bash
@@ -3,3 +3,18 @@
 #
 ## 
 
+export DIR="$(dirname "$(readlink -f "$0")")"
+
+export USERNAME="Zeph53"
+export REPONAME="Random-Scripts"
+export GITURL="https://github.com/$USERNAME/$REPONAME.git"
+
+cd $DIR ; 
+git init ; 
+git pull $GITURL main ; 
+git add * ; 
+git commit -m 'commit' ; 
+git branch -M main ; 
+git remote add origin $GITURL ; 
+git push -u origin main ;
+echo "DONE"
